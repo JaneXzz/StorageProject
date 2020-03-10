3d8e57216af976b5d267598d4adc1dc946c9b3fd
许珍珍 <xuzhenzhen@doudoubird.com>
2020-03-10 17:58:04 +0800

    删除重复文件

--- StorageProject/Classes/公共/NSDate+Decomposer.h
+++ StorageProject/Classes/公共/NSDate+Decomposer.h
@@ -10,7 +10,16 @@
 
 @interface NSDate (Decomposer)
 
-
+@property (readonly) NSInteger nearestHour;
+@property (readonly) NSInteger hour;
+@property (readonly) NSInteger minute;
+@property (readonly) NSInteger seconds;
+@property (readonly) NSInteger day;
+@property (readonly) NSInteger month;
+@property (readonly) NSInteger week;
+@property (readonly) NSInteger weekday;
+@property (readonly) NSInteger nthWeekday; // e.g. 2nd Tuesday of the month == 2
+@property (readonly) NSInteger year;
 /**
  *  获得当前 NSDate 对象对应的日子
  */
@@ -46,14 +55,53 @@
  */
 + (NSInteger)firstWeekDayOfMonth:(NSDate*)date;
 
+/**
+ *  根据当前时间的字符串获取相应的 NSDate
+ */
++ (NSDate *)date:(NSString *)datestr WithFormat:(NSString *)format;
+
+/**
+ *  根据时间的格式获取相应的时间NSDate
+ */
+- (NSDate *)dateWithFormatter:(NSString *)formatter;
+/**
+ *  根据时间的格式获取相应的时间字符串
+ */
+- (NSString *)stringWithFormat:(NSString *) format;
+
+/**
+ *  根据时间获取前一个月或者后一个月的时间 -1为前一个月  1为后一个月
+ */
++(NSDate *)getPriousorLaterDateFromDate:(NSDate *)date withMonth:(NSInteger)month;
 
 /**
  *  将string 转换为NSDate
  */
 +(NSDate *)stringConversionDate:(NSString *)dateStr;
+/**
+ *  将NSDate 转换为string
+ */
++(NSString *)dateConversionString:(NSDate *)date;
 
+/**
+ *  获得时分秒
+ */
++(NSString *)dateWithHHmmss;
 
-+(int)compareOneDay:(NSDate *)currentDay withAnotherDay:(NSDate *)BaseDay;
+/**
+ *  将时间的字符串转换为时间戳
+ */
++(NSInteger)getCurrentTimestamp:(NSString *)dateStr;
+
+/**
+ *  将时间戳转换为字符串
+ */
++(NSString *)timestampChangesToStandarTime:(uint64_t)timestamp;
+
+/**
+ *  获取当前时间的时间戳
+ */
++ (NSInteger)NSIntegerGetCurrentTimestamp;
 
 /**
 *  开始到截止的日期
@@ -62,6 +110,9 @@
 
 +(NSString *)strDaysFromDate:(NSDate *)startDate toDate:(NSDate *)endDate;
 
++ (NSString *)chindDateFormate:(NSDate *)date;
+
++(int)compareOneDay:(NSDate *)currentDay withAnotherDay:(NSDate *)BaseDay;
 
 
 @end
--- StorageProject/Classes/公共/NSDate+Decomposer.m
+++ StorageProject/Classes/公共/NSDate+Decomposer.m
@@ -8,6 +8,15 @@
 
 #import "NSDate+Decomposer.h"
 
+#define D_MINUTE    60
+#define D_HOUR        3600
+#define D_DAY        86400
+#define D_WEEK        604800
+#define D_YEAR        31556926
+
+static const unsigned componentFlags = (NSCalendarUnitYear| NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitWeekOfMonth |  NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond | NSCalendarUnitWeekday | NSCalendarUnitWeekdayOrdinal);
+
+
 @implementation NSDate (Decomposer)
 
 #pragma mark Public Method
@@ -86,11 +95,220 @@
     components.day = 15; // 定位到当月中间日子
     return components;
 }
+#pragma mark - Decomposing Dates
+
+- (NSInteger) nearestHour
+{
+    NSTimeInterval aTimeInterval = [[NSDate date] timeIntervalSinceReferenceDate] + D_MINUTE * 30;
+    NSDate *newDate = [NSDate dateWithTimeIntervalSinceReferenceDate:aTimeInterval];
+    NSDateComponents *components = [[NSDate currentCalendar] components:NSCalendarUnitHour fromDate:newDate];
+    return components.hour;
+}
+
+- (NSInteger) hour
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.hour;
+}
+
+- (NSInteger) minute
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.minute;
+}
+
+- (NSInteger) seconds
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.second;
+}
+
+- (NSInteger) day
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.day;
+}
+
+- (NSInteger) month
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.month;
+}
+
+- (NSInteger) week
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.weekOfMonth;
+}
+
+- (NSInteger) weekday
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.weekday;
+}
+
+- (NSInteger) nthWeekday // e.g. 2nd Tuesday of the month is 2
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.weekdayOrdinal;
+}
+
+- (NSInteger) year
+{
+    NSDateComponents *components = [[NSDate currentCalendar] components:componentFlags fromDate:self];
+    return components.year;
+}
+
++ (NSCalendar *) currentCalendar
+{
+    static NSCalendar *sharedCalendar = nil;
+    if (!sharedCalendar)
+        sharedCalendar = [NSCalendar autoupdatingCurrentCalendar];
+    return sharedCalendar;
+}
+
++ (NSDate *)date:(NSString *)datestr WithFormat:(NSString *)format
+{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setLocale:[NSLocale currentLocale]];
+    [dateFormatter setTimeZone:[NSTimeZone localTimeZone]];
+    [dateFormatter setDateFormat:format];
+    NSDate *date = [dateFormatter dateFromString:datestr];
+#if ! __has_feature(objc_arc)
+    [dateFormatter release];
+#endif
+    return date;
+}
+-(NSDate *)dateWithFormatter:(NSString *)formatter {
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = formatter;
+    NSString *selfStr = [fmt stringFromDate:self];
+    return [fmt dateFromString:selfStr];
+}
+- (NSString *)stringWithFormat:(NSString *) format
+{
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    //    formatter.locale = [NSLocale currentLocale]; // Necessary?
+    formatter.dateFormat = format;
+    return [formatter stringFromDate:self];
+}
+
++(NSDate *)getPriousorLaterDateFromDate:(NSDate *)date withMonth:(NSInteger)month
+{
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    [comps setMonth:month];
+    NSCalendar *calender = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDate *mDate = [calender dateByAddingComponents:comps toDate:date options:0];
+    return mDate;
+}
+
+//date转换为string
++(NSString *)dateConversionString:(NSDate *)date{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    NSTimeZone *tz = [NSTimeZone defaultTimeZone];
+    [dateFormatter setTimeZone:tz];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString* result=[dateFormatter stringFromDate:date];
+    return result;
+}
+
 //string转换为date
 +(NSDate *)stringConversionDate:(NSString *)dateStr{
-    NSDateFormatter *dateFormatter=[[NSDateFormatter alloc]init];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
     dateFormatter.dateFormat=@"yyyy-MM-dd HH:mm:ss";
     return [dateFormatter dateFromString:dateStr];
+}
+/**
+ *  获得时分秒
+ */
++(NSString *)dateWithHHmmss{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    NSTimeZone *tz = [NSTimeZone defaultTimeZone];
+    [dateFormatter setTimeZone:tz];
+    [dateFormatter setDateFormat:@"HH:mm:ss"];
+    NSString* result=[dateFormatter stringFromDate:[NSDate date]];
+    return result;
+}
+
+//将时间的字符串转换为时间戳
++(NSInteger)getCurrentTimestamp:(NSString *)dateStr{
+    //获取系统当前的时间戳
+    NSDate* dat = [NSDate stringConversionDate:dateStr];
+    NSTimeInterval a = [dat timeIntervalSince1970];
+    NSInteger time = round(a);
+    return time;
+}
+//获取系统当前的时间戳
++ (NSInteger)NSIntegerGetCurrentTimestamp{
+    //获取系统当前的时间戳
+    NSDate* dat = [NSDate dateWithTimeIntervalSinceNow:0];
+    NSTimeInterval a=[dat timeIntervalSince1970];
+    NSInteger time = round(a);
+    return time;
+}
+
+//将时间戳转换为字符串
++(NSString *)timestampChangesToStandarTime:(uint64_t)timestamp{
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate* date = [NSDate dateWithTimeIntervalSince1970:timestamp];
+    NSString* dateString = [formatter stringFromDate:date];
+    return dateString;
+}
+
++(NSInteger)daysFromDate:(NSDate *)startDate toDate:(NSDate *)endDate{
+
+   NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+   NSDateComponents *components = [gregorianCalendar components:(NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitDay)
+                                                       fromDate:startDate
+                                                         toDate:endDate
+                                                        options:0];
+   NSInteger days = labs([components day]);
+   NSInteger hours = labs([components hour]);
+   NSInteger minute = labs([components minute]);
+
+    if (days <= 0) {
+        if (hours >= 0) {
+            if (minute >= 0) {
+                return minute;
+            }
+            return hours;
+        }
+        return 0;
+    }else {
+        NSLog(@"%@",[[NSString alloc] initWithFormat:@"%li天%li小时",(long)days,(long)hours]);
+        return days;
+    }
+}
+
++(NSString *)strDaysFromDate:(NSDate *)startDate toDate:(NSDate *)endDate{
+
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *components = [gregorianCalendar components:(NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitDay)
+                                                        fromDate:startDate
+                                                          toDate:endDate
+                                                         options:0];
+    NSInteger days = labs([components day]);
+    NSInteger hours = labs([components hour]);
+    NSInteger minute = labs([components minute]);
+ 
+    if (days < 1) {
+        if (hours < 1) {
+            return [[NSString alloc] initWithFormat:@"%li分钟",(long)minute];
+        }
+        return [[NSString alloc] initWithFormat:@"%li小时",(long)hours];
+    }else{
+        return [[NSString alloc] initWithFormat:@"%li天",(long)days];
+    }
+}
+
++ (NSString *)chindDateFormate:(NSDate *)update{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *destDateString = [dateFormatter stringFromDate:update];
+    return destDateString;
 }
 
 +(int)compareOneDay:(NSDate *)currentDay withAnotherDay:(NSDate *)BaseDay{
@@ -114,44 +332,6 @@
     return 0;
 }
 
-+(NSInteger)daysFromDate:(NSDate *)startDate toDate:(NSDate *)endDate{
-    NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
-    // 话说在真机上需要设置区域，才能正确获取本地日期，天朝代码:zh_CN
-    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-    //得到相差秒数
-    NSTimeInterval time = [endDate timeIntervalSinceDate:startDate];
-    int days = ((int)time)/(3600*24);
-    int hours = ((int)time)%(3600*24)/3600;
-//    int minute = ((int)time)%(3600*24)/3600/60;
-    if (days <= 0) {
-        if (hours > 0) {
-            return hours;
-        }
-        return 0;
-    }else {
-        NSLog(@"%@",[[NSString alloc] initWithFormat:@"%i天%i小时",days,hours]);
-        return days;
-    }
-}
-
-+(NSString *)strDaysFromDate:(NSDate *)startDate toDate:(NSDate *)endDate{
-    NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
-    // 话说在真机上需要设置区域，才能正确获取本地日期，天朝代码:zh_CN
-    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"];
-    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-    //得到相差秒数
-    NSTimeInterval time = [endDate timeIntervalSinceDate:startDate];
-    int days = ((int)time)/(3600*24);
-    int hours = ((int)time)%(3600*24)/3600;
-//    int minute = ((int)time)%(3600*24)/3600/60;
-    if (days < 1) {
-        return [[NSString alloc] initWithFormat:@"%i小时",hours];
-    }else{
-        return [[NSString alloc] initWithFormat:@"%i天",days];
-    }
- 
-}
 
 
 @end
